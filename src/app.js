e2e4697068401f727cb550831c8983300be919dd
amityadav2321 <itsamityadav2307@gmail.com>
2025-06-21 19:56:11 +0530

    plaing with route

--- src/app.js
+++ src/app.js
@@ -6,13 +6,6 @@ app.get("/user",(req,res)=>{
     res.send({firstname:"Amit" , Lastname:"Yadav"});
 });
 
-app.post("/user",(req,res)=>{
-    res.send("Data saved successfully stored to database");
-});
-
-app.use("/user",(req,res)=>{
-    res.send("hahahahahahahahahaah")
-})
 app.listen(3000,()=>{
     console.log("server is running on 3000")
 }); 
