2bdb3b19d8722c365abeb49f6c96f989a5bed8c1
amityadav2321 <itsamityadav2307@gmail.com>
2025-06-21 18:54:59 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@ const express=require("express")
 
 const app=express();
 
-app.use("/test",(req,res)=>{
-    res.send("Hello  how are you ");
-})
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Amit" , Lastname:"Yadav"});
+});
 
+app.post("/user",(req,res)=>{
+    res.send("Data saved successfully stored to database");
+});
+
+app.use("/user",(req,res)=>{
+    res.send("hahahahahahahahahaah")
+})
 app.listen(3000,()=>{
     console.log("server is running on 3000")
-});
+}); 
