7e00b856f293e35f8505b36e078dde607dc453fe
amityadav2321 <itsamityadav2307@gmail.com>
2025-06-22 17:18:18 +0530

    playing with methods

--- src/app.js
+++ src/app.js
@@ -3,17 +3,11 @@ const connectDB =require("./config/database")
 const User=require("./models/user")
 const app=express();
 
+app.use(express.json());
 app.post("/signup",async(req,res)=>{
-  
+  console.log(req.body);
 //creating a new instance of a User model
-    const user = new User( {
-        firstName:"Amit",
-        lastName:"Yadav",
-        emailId:"amityadav@gmail.com",
-        password:"amit@123",
-        age:20,
-        gender:"Male"
-});
+    const user = new User( req.body);
     try{
          await user.save();
          res.send("Data has been saved...")
@@ -23,6 +17,61 @@ app.post("/signup",async(req,res)=>{
 
 })
 
+app.get("/user",async(req,res)=>{
+
+    const userEmail=req.body.emailId;
+    try{
+         const users= await User.find({
+        emailId:userEmail
+    })
+
+    if(users.length===0){
+        res.status(404).send("user not found");
+    }
+    else{
+        res.send(users);
+    }
+    }
+    catch(err){
+        res.status(400).send("something wrong");
+
+    }
+   
+})
+app.get("/feed",async(req,res)=>{
+     
+    try{
+        const users=await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("something wrong");
+
+    }
+});
+
+app.delete("/user",async(req,res)=>{
+     const userId=req.body.userId;
+     try{
+        const user= await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully");
+     }
+     catch(err){
+        res.status(404).send("something went wrong");
+     }
+})
+
+app.patch("/user",async(req,res)=>{
+     const userId=req.body.userId;
+     const data=req.body;
+
+     try {
+        await User.findByIdAndUpdate({_id:userId},data);
+        res.send("user updated successfully");
+     } catch (err) {
+        res.status(400).send("something error");
+     }
+})
 
 connectDB().then(()=>{
     console.log("Database connection established...");
