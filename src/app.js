554fb0188909433fb05b8a3b4c575ee85f095145
amityadav2321 <itsamityadav2307@gmail.com>
2025-06-22 15:12:10 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 const express=require("express")
-
+const connectDB =require("./config/database")
+const User=require("./models/user")
 const app=express();
 
-app.get("/user",(req,res)=>{
-    res.send({firstname:"Amit" , Lastname:"Yadav"});
+app.post("/signup",async(req,res)=>{
+  
+//creating a new instance of a User model
+    const user = new User( {
+        firstName:"Amit",
+        lastName:"Yadav",
+        emailId:"amityadav@gmail.com",
+        password:"amit@123",
+        age:20,
+        gender:"Male"
 });
+    try{
+         await user.save();
+         res.send("Data has been saved...")
+    }catch(err){
+        res.status(400).send("Error while saving data to database"+err.message)
+    }
 
-app.listen(3000,()=>{
+})
+
+
+connectDB().then(()=>{
+    console.log("Database connection established...");
+    app.listen(3000,()=>{
     console.log("server is running on 3000")
 }); 
+}).catch(err=>{
+    console.error("Database cannot be connected")
+})
+ 
