ef57364166838339325a59f30fa3804649914f0f
Amit Yadav <itsamityadav2307@gmail.com>
2025-06-24 23:57:32 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const connectDB= async()=>{
-    await mongoose.connect("mongodb+srv://amit_yadav2005:amityadav2005@cluster0.pkv06.mongodb.net/devTinder")
+    await mongoose.connect("mongodb+srv://Username:Password@cluster0.pkv06.mongodb.net/devTinder")
 }
 
 module.exports= connectDB;
